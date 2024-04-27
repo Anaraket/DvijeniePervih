2df97cffa23c85dce4e662136d7230f415d18e13
Anaraket <tulinovartem4@gmail.com>
2024-04-27 14:07:53 +0300

    Сертификат

--- utils/sertificate_create.py
+++ utils/sertificate_create.py
@@ -25,7 +25,7 @@ def certificate(fio, result):
         size=65)  # Шрифт для результата
     draw_text = ImageDraw.Draw(im)
     # Пишем ФИО. ХУ - находим середину поля ввода и середину ФИО и вычисляем как написать ФИО по центру поля ввода
-    draw_text.text(((((name_start[0] + name_end[0]) / 2) - width_name / 2) + 110, 730 + 135), text=str(fio),
+    draw_text.text(((((name_start[0] + name_end[0]) / 2) - width_name / 2) + 100, 730 + 135), text=str(fio),
                    font=font_fio,
                    fill='#000000', align="center")
     # Пишем результат, тем же самым образом ищем центр
